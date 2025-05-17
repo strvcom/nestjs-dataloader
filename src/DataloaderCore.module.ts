2e9d3aba4d18adbaef8e8a613cd59e326b963c47
Robert Rossmann <rr.rossmann@me.com>
2025-05-17 12:33:31 +0200

    chore: unify module's `.forRoot()` and `.forRootAsync()` code paths

--- src/DataloaderCore.module.ts
+++ src/DataloaderCore.module.ts
@@ -1,26 +1,14 @@
-import { type Provider, type DynamicModule, Module } from '@nestjs/common'
+import { type DynamicModule, Module } from '@nestjs/common'
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { DataloaderInterceptor } from './Dataloader.interceptor.js'
 import { OPTIONS_TOKEN } from './internal.js'
 import { type DataloaderModuleOptions, type DataloaderOptions } from './types.js'
 
 /** @private */
-const interceptor: Provider = { provide: APP_INTERCEPTOR, useClass: DataloaderInterceptor }
-
-/** @private */
 @Module({})
 class DataloaderCoreModule {
   static forRoot(options?: DataloaderOptions): DynamicModule {
-    return {
-      module: DataloaderCoreModule,
-      providers: [
-        interceptor,
-        {
-          provide: OPTIONS_TOKEN,
-          useValue: options,
-        },
-      ],
-    }
+    return this.forRootAsync({ useFactory: () => options ?? {} })
   }
 
   static forRootAsync(options: DataloaderModuleOptions): DynamicModule {
@@ -28,12 +16,8 @@ class DataloaderCoreModule {
       module: DataloaderCoreModule,
       imports: options.imports ?? [],
       providers: [
-        interceptor,
-        {
-          provide: OPTIONS_TOKEN,
-          inject: options.inject ?? [],
-          useFactory: options.useFactory,
-        },
+        { provide: APP_INTERCEPTOR, useClass: DataloaderInterceptor },
+        { provide: OPTIONS_TOKEN, inject: options.inject ?? [], useFactory: options.useFactory },
       ],
     }
   }
